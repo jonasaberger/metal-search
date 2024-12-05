728fbd5b7a02edc08daf4dce6249611313bf8505
AbergerJonas <jonas.aberger@htl-saalfelden.at>
2024-12-05 12:16:45 +0100

    Init Backend & Application

---
+++ MetalSearch/MetalSearch_Backend/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MetalSearch_Backend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** MetalSearch_Backend ***\n\n");
+
+            
+        }
+    }
+}
